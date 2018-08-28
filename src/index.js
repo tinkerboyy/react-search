c669c5deed3b5a1761618fd72b9cc0950ac0aa2e
Madhukar Tattikota <madhukart26@gmail.com>
2018-08-27 22:14:06 -0400

    updated root index file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import { withRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import store from './store';
+import { routerHistory } from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const AppWithRouter = withRouter(App);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={routerHistory}>
+      <AppWithRouter />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
